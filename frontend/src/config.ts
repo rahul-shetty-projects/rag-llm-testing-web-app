fca81f7e253a609fc37e67a29a14ca9e618872e4
satishccy <satishchoudhari13579@gmail.com>
2025-12-31 18:32:42 +0530

    build frontend build with prod backend url

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,2 +1,3 @@
-// export const BACKEND_URL = "https://qasummit.org:8080";
-export const BACKEND_URL = "http://127.0.0.1:8080";
+// export const BACKEND_URL = "https://qasummit.org:8080"; // qasummit.org backend
+export const BACKEND_URL = "https://rahulshettyacademy.com/rag-llm"; // production backend
+// export const BACKEND_URL = "http://127.0.0.1:8080"; // local backend
